d2909f79b321de0c31c29a2d40c7718ffa107e90
minhtr-coder <trichmse172870@fpt.edu.vn>
2025-02-14 23:31:28 +0700

    update

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -73,98 +73,45 @@ const Register = () => {
   };
 
   return (
-    <div className="slide-in-left">
-      <div className="register-container">
-        <div className="register-form">
-          <h2 className="register-title">Đăng ký</h2>
-          <div className="form-group">
-            <p>Họ và tên</p>
-            <input
-              type="text"
-              name="fullName"
-              value={formData.fullName}
-              placeholder="vd:Nguyễn Văn A"
-              className={`input-field ${errors.fullName ? "error" : ""}`}
-              onChange={handleInputChange}
-            />
-            {errors.fullName && <span className="error-message">{errors.fullName}</span>}
-          </div>
-          <div className="form-group">
-            <p>Email</p>
-            <input
-              type="text"
-              name="email"
-              value={formData.email}
-              placeholder="vd:ABC@gmail.com"
-              className={`input-field ${errors.email ? "error" : ""}`}
-              onChange={handleInputChange}
-            />
-            {errors.email && <span className="error-message">{errors.email}</span>}
-          </div>
-          <div className="form-group">
-            <p>Tài khoản</p>
-            <input
-              type="text"
-              name="username"
-              value={formData.username}
-              placeholder="vd:nguyenvana"
-              className={`input-field ${errors.username ? "error" : ""}`}
-              onChange={handleInputChange}
-            />
-            {errors.username && <span className="error-message">{errors.username}</span>}
-          </div>
-          <div className="form-group">
-            <p>Ngày sinh</p>
-            <input
-              type="date"
-              name="dateOfBirth"
-              value={formData.dateOfBirth}
-              className={`input-field ${errors.dateOfBirth ? "error" : ""}`}
-              onChange={handleInputChange}
-            />
-            {errors.dateOfBirth && <span className="error-message">{errors.dateOfBirth}</span>}
-          </div>
-          <div className="form-group">
-            <p>CCCD</p>
-            <input
-              type="text"
-              name="cccd"
-              value={formData.cccd}
-              placeholder="vd:012345678912"
-              className={`input-field ${errors.cccd ? "error" : ""}`}
-              onChange={handleInputChange}
-            />
-            {errors.cccd && <span className="error-message">{errors.cccd}</span>}
-          </div>
-          <div className="form-group">
-            <p>Mật khẩu</p>
-            <input
-              type="password"
-              name="password"
-              value={formData.password}
-              placeholder="vd:12345678"
-              className={`input-field ${errors.password ? "error" : ""}`}
-              onChange={handleInputChange}
-            />
-            {errors.password && <span className="error-message">{errors.password}</span>}
-          </div>
-          <div className="form-group">
-            <p>Xác nhận mật khẩu</p>
-            <input
-              type="password"
-              name="confirmPassword"
-              value={formData.confirmPassword}
-              placeholder="vd:12345678"
-              className={`input-field ${errors.confirmPassword ? "error" : ""}`}
-              onChange={handleInputChange}
-            />
-            {errors.confirmPassword && <span className="error-message">{errors.confirmPassword}</span>}
-          </div>
-          <button type="submit" className={`register-button ${ActiveTab === "register" ? "active-class" : ""}`} onClick={handleRegister}>Đăng ký</button>
-          <p>Đã có tài khoản? <button><Link to="/login" className="login-reg-button">Đăng nhập ngay</Link></button></p>
-        </div>
-        <div className="picture">Picture</div>
+    <div className="register-page">
+      <h1 className="register-title">Đăng ký</h1>
+      <div className="form-group">
+        <p>Họ và tên</p>
+        <input type="text" name="fullName" value={formData.fullName} placeholder="vd:Nguyễn Văn A" className={`form-control ${errors.fullName ? "is-invalid" : ""}`} onChange={handleInputChange} />
+        {errors.fullName && <span className="invalid-feedback">{errors.fullName}</span>}
       </div>
+      <div className="form-group">
+        <p>Email</p>
+        <input type="email" name="email" value={formData.email} placeholder="vd:ABC@gmail.com" className={`form-control ${errors.email ? "is-invalid" : ""}`} onChange={handleInputChange} />
+        {errors.email && <span className="invalid-feedback">{errors.email}</span>}
+      </div>
+      <div className="form-group">
+        <p>Tài khoản</p>
+        <input type="text" name="username" value={formData.username} placeholder="vd:nguyenvana" className={`form-control ${errors.username ? "is-invalid" : ""}`} onChange={handleInputChange} />
+        {errors.username && <span className="invalid-feedback">{errors.username}</span>}
+      </div>
+      <div className="form-group">
+        <p>Ngày sinh</p>
+        <input type="date" name="dateOfBirth" value={formData.dateOfBirth} className={`form-control ${errors.dateOfBirth ? "is-invalid" : ""}`} onChange={handleInputChange} />
+        {errors.dateOfBirth && <span className="invalid-feedback">{errors.dateOfBirth}</span>}
+      </div>
+      <div className="form-group">
+        <p>CCCD</p>
+        <input type="text" name="cccd" value={formData.cccd} placeholder="vd:012345678912" className={`form-control ${errors.cccd ? "is-invalid" : ""}`} onChange={handleInputChange} />
+        {errors.cccd && <span className="invalid-feedback">{errors.cccd}</span>}
+      </div>
+      <div className="form-group">
+        <p>Mật khẩu</p>
+        <input type="password" name="password" value={formData.password} placeholder="vd:12345678" className={`form-control ${errors.password ? "is-invalid" : ""}`} onChange={handleInputChange} />
+        {errors.password && <span className="invalid-feedback">{errors.password}</span>}
+      </div>
+      <div className="form-group">
+        <p>Xác nhận mật khẩu</p>
+        <input type="password" name="confirmPassword" value={formData.confirmPassword} placeholder="vd:12345678" className={`form-control ${errors.confirmPassword ? "is-invalid" : ""}`} onChange={handleInputChange} />
+        {errors.confirmPassword && <span className="invalid-feedback">{errors.confirmPassword}</span>}
+      </div>
+      <button type="submit" className="btn btn-primary" onClick={handleRegister}>Đăng ký</button>
+      <p>Đã có tài khoản? <button><Link to="/login" className="btn btn-link">Đăng nhập ngay</Link></button></p>
     </div>
   );
 };
