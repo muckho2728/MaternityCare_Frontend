24cef1119cf792c1cf0e2e7d448cb59bd75c7a86
Cao Hong My Duyen <148830357+midin1507@users.noreply.github.com>
2025-02-23 23:04:27 +0700

    update ui

--- src/pages/PackageList/PackageList.jsx
+++ src/pages/PackageList/PackageList.jsx
@@ -1,29 +1,37 @@
 import React from 'react';
 import { Card, Col, Row, Button } from 'antd';
-import './PackageList.css'; // Import file CSS
+import { useNavigate } from 'react-router-dom'; // Import useNavigate
+import './PackageList.css'; 
 
 const PackageList = () => {
-  // Dữ liệu mẫu để hiển thị
+  const navigate = useNavigate(); // Khởi tạo useNavigate
+
   const sampleData = [
-    { id: 1, name: 'Basic Package', description: 'Basic features', price: '$10', duration: '1 Month' },
-    { id: 2, name: 'Standard Package', description: 'Standard features', price: '$20', duration: '1 Month' },
-    { id: 3, name: 'Premium Package', description: 'All features', price: '$30', duration: '1 Month' },
+    { id: 1, name: 'Gói Theo Dõi Cơ Bản', price: '10 US$/tháng', features: ['Cập nhật hàng tuần về sự phát triển của thai nhi', 'Bài viết hướng dẫn chăm sóc sức khỏe', 'Nhắc nhở lịch khám thai', 'Diễn đàn hỗ trợ từ các chuyên gia'] },
+    { id: 2, name: 'Gói Theo Dõi Cao Cấp', price: '30 US$/tháng', features: ['Tất cả tính năng của Gói Cơ Bản', 'Tư vấn cá nhân từ chuyên gia dinh dưỡng', 'Khóa học trực tuyến về dinh dưỡng và thể dục cho mẹ bầu', 'Truy cập vào các bài viết chuyên sâu và video hướng dẫn'] },
   ];
+
+  const handleBuyClick = () => {
+    navigate('/payment-detail'); // Chuyển hướng đến trang PaymentDetail
+  };
 
   return (
     <div className="package-list">
       <header className="package-list-header">
-        <h1>Available Packages</h1>
+        <h2>Danh sách gói</h2>
       </header>
       <main className="package-list-content">
-        <Row gutter={16}>
+        <Row gutter={16} justify="center">
           {sampleData.map(pkg => (
             <Col span={8} key={pkg.id}>
-              <Card title={pkg.name} bordered={false} className="package-card">
-                <p>{pkg.description}</p>
-                <p>Price: {pkg.price}</p>
-                <p>Duration: {pkg.duration}</p>
-                <Button type="primary">Buy</Button> 
+              <Card title={pkg.name} bordered={true} className="package-card">
+                <p className="package-price">Giá: {pkg.price}</p>
+                <ul>
+                  {pkg.features.map((feature, index) => (
+                    <li key={index}>{feature}</li>
+                  ))}
+                </ul>
+                <Button type="primary" onClick={handleBuyClick}>Mua</Button> {/* Thêm onClick */}
               </Card>
             </Col>
           ))}
