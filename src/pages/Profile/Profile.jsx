c596596b66a68e40c1d63563e717f9e59980a757
LouisVu415904 <minhvhse182677@fpt.edu.vn>
2025-03-24 23:32:47 +0700

    sap xong r nhe

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Layout, Menu, Input, Button, Form, Typography, Card, Row, Col, Space, message, Upload, Table } from 'antd';
-import { UserOutlined, HeartOutlined, EditOutlined, CameraOutlined, MessageOutlined } from '@ant-design/icons';
+import { UserOutlined, HeartOutlined, EditOutlined, CameraOutlined, MessageOutlined, BookOutlined } from '@ant-design/icons';
 import { updateUserByIdAction, changePassworbyUserIdAction, fetchUserByIdAction } from '../../store/redux/action/userAction';
 import api from '../../config/api';
 import { Link } from 'react-router-dom';
@@ -38,7 +38,7 @@ const Profile = () => {
   const fetchUserPackage = async () => {
     try {
       const response = await api.get(`/authentications/current-user`, {
-        headers: { Authorization: `Bearer ${token}` }, 
+        headers: { Authorization: `Bearer ${token}` },
       });
       const subscription = response.data.subscription;
 
@@ -53,7 +53,7 @@ const Profile = () => {
   const fetchSubscriptionDetails = async () => {
     try {
       const response = await api.get(`/users/${userId}/subscriptions`, {
-        headers: { Authorization: `Bearer ${token}` }, 
+        headers: { Authorization: `Bearer ${token}` },
       });
       setSubscriptionDetails(response.data);
     } catch (error) {
@@ -187,52 +187,52 @@ const Profile = () => {
 
   return (
     <Layout style={{ backgroundColor: '#f0f2f5' }}>
-      <Content style={{ padding: '24px', maxWidth: '1200px', margin: '0 auto', background:'#f2fffa8f' }}>
+      <Content style={{ padding: '24px', maxWidth: '1200px', margin: '0 auto', background: '#f2fffa8f' }}>
         <Row gutter={24}>
           <Col span={6}>
-                <Card span={0} style={{ textAlign: 'center'}}>
-                  <Title level={3} style={{ color: '#EC407A' }}>Ảnh đại diện</Title>
-                  <div style={{ position: 'relative', margin: '0 auto', width: '150px', height: '150px' }}>
-                    <img
-                      src={previewAvatar || 'https://via.placeholder.com/150?text=Avatar'}
-                      alt="avatar"
+            <Card span={0} style={{ textAlign: 'center' }}>
+              <Title level={3} style={{ color: '#EC407A' }}>Ảnh đại diện</Title>
+              <div style={{ position: 'relative', margin: '0 auto', width: '150px', height: '150px' }}>
+                <img
+                  src={previewAvatar || 'https://via.placeholder.com/150?text=Avatar'}
+                  alt="avatar"
+                  style={{
+                    width: '150px',
+                    height: '150px',
+                    borderRadius: '50%',
+                    objectFit: 'cover',
+                    border: '1px solid #EC407A',
+                  }}
+                />
+                {isEditing && (
+                  <Upload
+                    name="avatar"
+                    showUploadList={false}
+                    beforeUpload={(file) => {
+                      handleAvatarChange({ file });
+                      return false;
+                    }}
+                  >
+                    <Button
+                      icon={<CameraOutlined />}
                       style={{
-                        width: '150px',
-                        height: '150px',
+                        position: 'absolute',
+                        bottom: '0',
+                        right: '0',
                         borderRadius: '50%',
-                        objectFit: 'cover',
-                        border: '1px solid #EC407A',
+                        backgroundColor: '#1890ff',
+                        color: 'white',
+                        border: 'none',
                       }}
                     />
-                    {isEditing && (
-                      <Upload
-                        name="avatar"
-                        showUploadList={false}
-                        beforeUpload={(file) => {
-                          handleAvatarChange({ file });
-                          return false;
-                        }}
-                      >
-                        <Button
-                          icon={<CameraOutlined />}
-                          style={{
-                            position: 'absolute',
-                            bottom: '0',
-                            right: '0',
-                            borderRadius: '50%',
-                            backgroundColor: '#1890ff',
-                            color: 'white',
-                            border: 'none',
-                          }}
-                        />
-                      </Upload>
-                    )}
-                  </div>
-                  <Title level={4}>{userDetailData?.fullName || userLogin?.fullName}</Title>
-                  <p>{userDetailData?.email || userLogin?.email}</p>
-                  <p>{userLogin?.phone}</p>
-                  <p>{userLogin?.address}</p>
-                </Card>
+                  </Upload>
+                )}
+              </div>
+              <Title level={4}>{userDetailData?.fullName || userLogin?.fullName}</Title>
+              <p>{userDetailData?.email || userLogin?.email}</p>
+              <p>{userLogin?.phone}</p>
+              <p>{userLogin?.address}</p>
+            </Card>
             <Card
               style={{
                 borderRadius: '10px',
@@ -246,6 +246,7 @@ const Profile = () => {
                 { key: '2', icon: <HeartOutlined />, label: <Link to="/view-fetus-health">Xem thông tin sức khỏe</Link> },
                 { key: '3', icon: <MessageOutlined />, label: <Link to="/manage-pregnancy">Quản lý thông tin thai kỳ</Link> },
                 { key: '4', icon: <MessageOutlined />, label: <Link to="/manage-preg">Quản lý thai kỳ</Link> },
+                { key: '5', icon: <BookOutlined />, label: <Link to="/viewBookedSlot">Xem lịch đã đặt</Link> }
               ]} />
             </Card>
           </Col>
@@ -260,7 +261,7 @@ const Profile = () => {
               }}
             >
               <Row gutter={24}>
-                
+
 
                 <Col span={24}>
                   <Title level={3} style={{ color: '#EC407A' }}>Thông tin cá nhân</Title>
